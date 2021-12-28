4615f8e51e8b3683077bd21408ddd67103620ba9
bujosa <davidbujosa@gmail.com>
2021-12-28 07:50:15 +0100

    Refactor storage class

--- models/storages.js
+++ models/storages.js
@@ -9,19 +9,7 @@ class Storages {
   }
 
   execute() {
-    // Listen for new messages in this subscription
-    this.pubsub
-      .subscription("capella-b-testing-topic-2")
-      .on("message", (message) => {
-        // Example extracting data for the message
-        const { your_variables } = JSON.parse(message.data.toString());
-
-        // In this part you can specify what you want to do with the message
-        // For example you can save the message in a database
-        // Or you can send an email to the user
-
-        message.ack();
-      });
+    this.storage.getBuckets().then((results) => console.log(results));
   }
 }
 
