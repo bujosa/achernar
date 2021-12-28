5d2374f1120301272b7d52da188026c77a25ca62
bujosa <davidbujosa@gmail.com>
2021-12-28 11:10:15 +0100

    Refactor index

--- index.js
+++ index.js
@@ -8,12 +8,3 @@ server.execute();
 
 const storages = new Storages();
 storages.execute();
-
-// // Functions
-// function decodeBase64Json(data) {
-//   return JSON.parse(Buffer.from(data, "base64").toString());
-// }
-
-// function uploadPicture(image) {
-//   console.log("Upload picture");
-// }
