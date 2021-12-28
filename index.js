314ae29d818ad2f2851afbafc5d1e1cbb6835779
bujosa <davidbujosa@gmail.com>
2021-12-28 12:43:34 +0100

    Refactor index

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 const Server = require("./models/server");
-const Storages = require("./models/storages");
 
 require("dotenv").config();
 
 const server = new Server();
 server.execute();
-
-const storages = new Storages();
-storages.execute();
