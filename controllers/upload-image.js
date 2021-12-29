05103040b6e570b8babe5776f21dd1d49001facc
bujosa <davidbujosa@gmail.com>
2021-12-29 08:11:19 +0100

    Add image url

--- controllers/upload-image.js
+++ controllers/upload-image.js
@@ -1,15 +1,16 @@
 const { response } = require("express");
-const { Storages } = require("../models/storages");
+const Storages = require("../models/storages");
 
 const uploadImage = async (req, res = response) => {
   const { file, filename, email } = decodeBase64Json(req.body.message.data);
   try {
-    const storage = new Storages();
-    const imageUrl = await storage.upload(file, filename);
-    console.log(`Upload your picture`, imageUrl);
+    const storages = new Storages();
+    const imageUrl = await storages.upload(file, filename);
+
     res.status(200).send("OK");
   } catch (e) {
     console.log(e);
+
     res.status(500).send("Error");
   }
 };
