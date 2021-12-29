57ad428b8bc30e58c279494360e1e8d6d79e9cab
bujosa <davidbujosa@gmail.com>
2021-12-29 12:56:20 +0100

    Add limit mb

--- models/server.js
+++ models/server.js
@@ -14,8 +14,8 @@ class Server {
   }
 
   middlewares() {
-    this.app.use(express.urlencoded({ extended: true }));
-    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+    this.app.use(express.json({ limit: "50mb" }));
 
     // End Points
     this.app.use("/", require("../router/upload-image"));
