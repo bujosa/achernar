3e082e43f70d6d6b8b06acbcf95ec01c8a5412a1
bujosa <davidbujosa@gmail.com>
2021-12-29 13:22:39 +0100

    Add pubsub model

---
+++ models/pubsub.js
@@ -0,0 +1,12 @@
+const { PubSub } = require("@google-cloud/pubsub");
+const pubsub = new PubSub({});
+
+// Publish message to PubSub
+const publishPubSubMessage = async function (topicName, data) {
+  const dataBuffer = Buffer.from(JSON.stringify(data));
+  await pubsub.topic(topicName).publishMessage({
+    data: dataBuffer,
+  });
+};
+
+module.exports = { publishPubSubMessage };
